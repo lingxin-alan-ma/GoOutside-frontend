413e0fa036271967621c26fde98c2c51ac02425c
zcq <zcq2542@gmail.com>
2022-08-04 18:13:43 -0400

    add upload image function

--- src/components/AddActiv.js
+++ src/components/AddActiv.js
@@ -5,75 +5,77 @@ import { Form, Button, Container } from "react-bootstrap";
 
 
 import S3 from 'react-aws-s3';
-
-window.Buffer = window.Buffer || require("buffer").Buffer;
+import Upload from "./Upload";
+// window.Buffer = window.Buffer || require("buffer").Buffer;
 
 const AddActiv = ({ user }) => {
-  const navigate = useNavigate();
-  let params = useParams();
+  // const navigate = useNavigate();
+  // let params = useParams();
 
-  const [name, setName] = useState();
-  const [address, setAddress] = useState([]);
-  const [description, setDescription] = useState();
-  const [imagesUrl, setImagesUrl] = useState();
-  const [imageFile, setImageFile] = useState();
-  const [tags, setTags] = useState([]);
+  // const [name, setName] = useState();
+  // const [address, setAddress] = useState([]);
+  // const [description, setDescription] = useState();
+  // const [imagesUrl, setImagesUrl] = useState();
+  // const [imageFile, setImageFile] = useState(null);
+  // const [tags, setTags] = useState([]);
 
 
-  const config = {
-    bucketName: process.env.REACT_APP_BUCKET_NAME,
-    //dirName: 'media', /* optional */
-    region: process.env.REACT_APP_REGION,
-    accessKeyId: process.env.REACT_APP_ACCESS,
-    secretAccessKey: process.env.REACT_APP_SECRET,
-    //s3Url: 'https:/your-custom-s3-url.com/', /* optional */
-  }
+  // const config = {
+  //   bucketName: process.env.REACT_APP_BUCKET_NAME,
+  //   //dirName: 'media', /* optional */
+  //   region: process.env.REACT_APP_REGION,
+  //   accessKeyId: process.env.REACT_APP_ACCESS,
+  //   secretAccessKey: process.env.REACT_APP_SECRET,
+  //   //s3Url: 'https:/your-custom-s3-url.com/', /* optional */
+  // }
 
-  const ReactS3Client = new S3(config);
+  //const ReactS3Client = new S3(config);
   /*  Notice that if you don't provide a dirName, the file will be automatically uploaded to the root of your bucket */
 
-  /* This is optional */
-  const newFileName = "name";
+  // /* This is optional */
+  // const newFileName = "name";
 
 
-  const handleFileInput = (e) => {
-    e.preventDefault();
-    setImageFile(e.target.files[0]);
-  }
+  // const handleFileInput = (e) => {
+  //   e.preventDefault();
+  //   setImageFile(e.target.files[0]);
+  // }
 
 
-  const uploadPic = async (imageFile) => {
-    ReactS3Client
-      .uploadFile(imageFile, newFileName)
-      .then(data => {
-        console.log(data);
-        setImagesUrl(data.Location);
-      })
-      .catch(err => console.error(err));
-  }
+  // const uploadPic = async (imageFile) => {
+  //   const ReactS3Client = new S3(config);
+  //   console.log(imageFile.name);
+  //   ReactS3Client
+  //     .uploadFile(imageFile, imageFile.name)
+  //     .then(data => {
+  //       console.log(data.Location);
+  //       //setImagesUrl(data.Location);
+  //     })
+  //     .catch(err => console.error(err));
+  // }
 
 
-  const saveActiv = () => {
-    var data = {
-      user_id: user.googleId,
-      name: name,
-      address: address,
-      imagesUrl: imagesUrl,
-      description: description,
-      tags: tags
-    }
-    ActivDataService.creatActiv(data)
-      .then(response => {
-        navigate("/activs/" + params.id)
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
+  // const saveActiv = () => {
+  //   var data = {
+  //     user_id: user.googleId,
+  //     name: name,
+  //     address: address,
+  //     imagesUrl: imagesUrl,
+  //     description: description,
+  //     tags: tags
+  //   }
+  //   ActivDataService.creatActiv(data)
+  //     .then(response => {
+  //       //navigate("/activs/" + params.id)
+  //     })
+  //     .catch(e => {
+  //       console.log(e);
+  //     });
+  // }
 
   return (
     <Container className="main-container">
-      <Form>
+      {/* <Form>
         <Form.Group className="mb-3">
           <Form.Label>{"Create"} activity</Form.Label>
           <br></br>
@@ -93,7 +95,7 @@ const AddActiv = ({ user }) => {
             type="file"
             onChange={handleFileInput}
           />
-          <button onClick={() => {uploadPic(imageFile)}}>Upload</button>
+          <button onClick={() => {console.log(imageFile.name);uploadPic(imageFile)}}>Upload</button>
           <br></br>
           <input
             value={description}
@@ -111,7 +113,9 @@ const AddActiv = ({ user }) => {
 
       <Button variant="primary" onClick={saveActiv}>
         Submit
-      </Button>
+      </Button> */}
+      <Upload></Upload>
+      
     </Container>
   )
 }
