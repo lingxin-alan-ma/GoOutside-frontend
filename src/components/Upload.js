a1d3fdba3947e9d8957342c38fab85846ea49857
zcq <zcq2542@gmail.com>
2022-08-10 19:53:09 -0400

    adding delete image function

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,5 +1,6 @@
 import React , {useState} from 'react';
 import S3 from 'react-aws-s3';
+import FileHeader from "./FileHeader";
 
 // installed using npm install buffer --save
 window.Buffer = window.Buffer || require("buffer").Buffer;
@@ -31,20 +32,38 @@ window.Buffer = window.Buffer || require("buffer").Buffer;
         setSelectedFile(e.target.files[0]);
     }
 
+    const ReactS3Client = new S3(config);
+
     const uploadFile = async (file) => {
-        const ReactS3Client = new S3(config);
-        // the name of the file uploaded is used to upload it to S3
-        ReactS3Client
-        .uploadFile(file, file.name)
-        .then(data => {console.log(data.location); addImageUrl(data.location);})
-        .catch(err => console.error(err))
+      // the name of the file uploaded is used to upload it to S3
+      ReactS3Client
+      .uploadFile(file, file.name)
+      .then(data => {console.log(data.location); addImageUrl(data.location);})
+      .catch(err => console.error(err));
     }
-    return <div>
+
+    const deleteFile = async (fileUrl) => {
+      let filename = fileUrl.slice(45);
+      console.log(filename);
+      ReactS3Client
+      .deleteFile(filename)
+      .then(response => {console.log(response); deleteImageUrl(fileUrl)})
+      .catch(err => console.error(err));
+    }
+
+    return(
+      <div>
         <div>Image Upload</div>
         <input type="file" onChange={handleFileInput}/>
         <br></br>
         <button onClick={() => uploadFile(selectedFile)}> Upload </button>
-    </div>
+        {ImageUrl.map((url) => {
+          return (
+            <FileHeader url={url} deleteFile={deleteFile}></FileHeader>
+          )
+        })}
+      </div>
+    )
 }
 
 export default Upload;
